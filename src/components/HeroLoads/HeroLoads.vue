8a20afcfbf0910c5ef2bc1920448230ade4112f3
kotetratata <kotetratata@gmail.com>
2020-05-05 10:26:53 +0300

    add accept to input

--- src/components/HeroLoads/HeroLoads.vue
+++ src/components/HeroLoads/HeroLoads.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="file" @change="setFile" class="hero-loads__input">
+    <input type="file" @change="setFile" class="hero-loads__input" accept=".jpg, .jpeg, .png">
     <ol>
       <li v-for="(file, index) in files" :key="index">{{file}}</li>
     </ol>
