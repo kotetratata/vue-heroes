7eed4b0e5fd0fb064cbd68d2122ff358627cfead
kotetratata <kotetratata@gmail.com>
2020-05-18 20:22:57 +0300

    for pr

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="app" v-if="getLogStatus">
     <Header></Header>
-    <div class="nav">
-      <router-link class="nav__item" to="/">Home</router-link> |
-      <router-link class="nav__item" to="/about">About</router-link>
-      <p @click="logOutUser">Exit</p>
-    </div>
+    <Navigation></Navigation>
     <router-view/>
     <Footer></Footer>
   </div>
